54d31c1bbea97e14e3c736fb760fa6ab8d862b04
Deepak <deepak3823@gmail.com>
2017-05-04 22:48:23 +0530

    Added $.getJSON() function

--- js/script.js
+++ js/script.js
@@ -22,26 +22,37 @@ function loadData() {
         'api-key' : "8bab2b2e1cea4d7893a2284117356ba2",
         'q' : street + ',' + city
     });
-    $.ajax({
-        url : ny_url,
-        method : 'GET',
-    }).done(function(result){
-        // console.log(result.response.docs[0].headline.main);
-        // console.log(result.response.docs[0].web_url);
-        // console.log(result.response.docs[0].snippet);
+    // $.ajax({
+    //     url : ny_url,
+    //     method : 'GET',
+    // }).done(function(result){
+    //     // console.log(result.response.docs[0].headline.main);
+    //     // console.log(result.response.docs[0].web_url);
+    //     // console.log(result.response.docs[0].snippet);
+    //     var ny_articles = '' ;
+    //     for (var i = 0; i < result.response.docs.length; i++) {
+    //         ny_articles += '<a href="' + result.response.docs[i].web_url + '">' + result.response.docs[i].headline.main + '</a>'
+    //         ny_articles += '<p>' + result.response.docs[i].snippet + '</p>'
+    //         // console.log(result.response.docs[i].headline.main);
+    //         // console.log(result.response.docs[i].web_url);
+    //         // console.log(result.response.docs[i].snippet);
+    //     }
+    //     $('.nytimes-container').append(ny_articles);
+    // }).fail(function(){
+    //     console.log('Error occured while fetching ny times article.');
+    // })
+    $.getJSON(ny_url, function(result){
+        // console.log(result);
         var ny_articles = '' ;
-        for (var i = 0; i < result.response.docs.length; i++) {
-            ny_articles += '<a href="' + result.response.docs[i].web_url + '">' + result.response.docs[i].headline.main + '</a>'
-            ny_articles += '<p>' + result.response.docs[i].snippet + '</p>'
-            // console.log(result.response.docs[i].headline.main);
-            // console.log(result.response.docs[i].web_url);
-            // console.log(result.response.docs[i].snippet);
+    for (var i = 0; i < result.response.docs.length; i++) {
+        ny_articles += '<a href="' + result.response.docs[i].web_url + '">' + result.response.docs[i].headline.main + '</a>'
+        ny_articles += '<p>' + result.response.docs[i].snippet + '</p>'
+        // console.log(result.response.docs[i].headline.main);
+        // console.log(result.response.docs[i].web_url);
+        // console.log(result.response.docs[i].snippet);
         }
         $('.nytimes-container').append(ny_articles);
-    }).fail(function(){
-        console.log('Error occured while fetching ny times article.');
     })
-
     return false;
 };
 
